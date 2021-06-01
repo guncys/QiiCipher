95f2847633bc15a25f60aa89159a776b1ca617b1
Yuki Shimada <emadurandal@gmail.com>
2021-06-01 20:31:06 +0900

    Add the archive.bat and dearchive.bat for GUI drag & drop operation

---
+++ gui/archive.bat
@@ -0,0 +1,42 @@
+set Args=%*
+set Args=%Args:"=\"%
+
+@powershell -NoProfile -ExecutionPolicy Unrestricted "&([scriptblock]::create((gc \"%~f0\"|?{$_.readcount -gt 7})-join\"`n\"))" '%Args%'
+goto:eof
+
+[void]([System.Reflection.Assembly]::LoadWithPartialName("Microsoft.VisualBasic"))
+$path = [string]$args;
+
+Add-type -AssemblyName System.Web
+Add-Type -AssemblyName Microsoft.VisualBasic
+$defaultPassword = [System.Web.Security.Membership]::GeneratePassword(20,3)
+$githubId = [Microsoft.VisualBasic.Interaction]::InputBox('Enter the Github userid who you want to send a file', 'Github user id')
+
+$wslpath=wsl --exec wslpath -u $path
+$filename=[System.IO.Path]::GetFileName($path)
+$parentpath=Split-Path -Path $path
+$wslppath=wsl --exec wslpath -u $parentpath
+$passwordpath= $parentpath + "/" + $filename + "-archive/" + $filename + ".tar.gz.aes.passwd"
+$wslpasspath=wsl --exec wslpath -u $passwordpath
+
+
+#Write-Host $wslppath
+#Write-Host $githubId
+#Write-Host $wslpasspath
+
+$encPath = $env:QIICIPHER_BIN + "enc"
+$archivePath = $env:QIICIPHER_BIN + "archive"
+
+
+# Archive
+$archiveCmd="cd "+ $wslppath + " & " + $archivePath + " " + $filename
+#Write-Host $archiveCmd
+wsl bash -c $archiveCmd
+
+
+# Encrypt shared key password
+$encCmd=$encPath + " " + $githubId + " " + $wslpasspath
+#Write-Host $encCmd
+wsl bash -c $encCmd
+
+pause
---
+++ gui/dearchive.bat
@@ -0,0 +1,47 @@
+set Args=%*
+set Args=%Args:"=\"%
+
+@powershell -NoProfile -ExecutionPolicy Unrestricted "&([scriptblock]::create((gc \"%~f0\"|?{$_.readcount -gt 7})-join\"`n\"))" '%Args%'
+goto:eof
+
+[void]([System.Reflection.Assembly]::LoadWithPartialName("Microsoft.VisualBasic"))
+$path = [string]$args;
+
+Add-type -AssemblyName System.Web
+Add-Type -AssemblyName Microsoft.VisualBasic
+$defaultPassword = [System.Web.Security.Membership]::GeneratePassword(20,3)
+
+$privatekeypath=$env:QIICIPHER_GITHUB_KEYNAME
+if([string]::IsNullOrEmpty($privatekeypath)) {
+    $privatekeypath = [Microsoft.VisualBasic.Interaction]::InputBox('Enter the path of github private key', 'private key path')
+}
+
+$wslpath=wsl --exec wslpath -u $path
+$filename=[System.IO.Path]::GetFileName($path)
+$parentpath=Split-Path -Path $path
+$wslppath=wsl --exec wslpath -u $parentpath
+$inputpath= $wslppath + "/" + $filename
+$cryptedpasswordpath= $wslppath + "/" + $filename + ".passwd.enc"
+$passwordpath= $wslppath + "/" + $filename + ".passwd"
+$outputpath= $wslppath + "/" + $filename -replace ".tar.gz.aes",""
+$wslpasspath=wsl --exec wslpath -u $passwordpath
+
+
+#Write-Host $wslppath
+#Write-Host $githubId
+#Write-Host $wslpasspath
+
+$dearchivePath = $env:QIICIPHER_BIN + "dearchive"
+$decPath = $env:QIICIPHER_BIN + "dec"
+
+# Decrypt share key password
+$decCmd=$decPath + " " + $privatekeypath + " " + $cryptedpasswordpath + " " + $passwordpath
+Write-Host $decCmd
+wsl bash -c $decCmd
+
+# Dearchive
+$dearchiveCmd=$dearchivePath + " " + $inputpath + " " + $outputpath + " " + $passwordpath
+Write-Host $dearchiveCmd
+wsl bash -c $dearchiveCmd
+
+pause
